ec4cd0afe0a195c18a216e78f81c571f475743c9
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-10-31 01:10:57 +0900

    :recycle: refactoring syntax

---
+++ modules/syntax/ccpp/ccpp.h
@@ -0,0 +1,38 @@
+#ifndef _SYNTAX_CCPP_H
+#define _SYNTAX_CCPP_H
+
+// C and C++
+char *CCPP_extensions[] = {".c", ".cpp", ".h", NULL};
+char *CCPP_keywords[] = {
+        //types and misc
+        "char", "bool", "short", "int", "__int8", "__int16", "__int32", "__int64",
+        "long", "wchar_t", "__wchar_t", "float", "double", "true", "false",
+        "continue", "break", "enum", "struct", "class", "default#", "namespace",
+
+        //preprocessor
+        "#define|", "#elif|", "#else|", "#ifndef|", "#error|", "#if|", "#ifdef|",
+        "#pragma|", "#import|", "#include|", "#line|", "#undef|", "#using|", "#endif|",
+
+        //conditionals
+        "if~", "else~", "switch~", "case~", "try~", "throw~", "catch~",
+
+        //return
+        "return#", "goto#",
+
+        //adapters
+        "const^", "static^", "public^", "protected^", "void^", "typedef^",
+        "union^", "virtual^", "volatile^", "private^",
+
+        //loops,
+        "for@", "while@", "do@",
+        NULL
+};
+
+#define CCPP_syntax { \
+    CCPP_extensions, \
+    CCPP_keywords, \
+    "//", \
+    "/*", \
+    "*/", \
+    HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS \
+}
